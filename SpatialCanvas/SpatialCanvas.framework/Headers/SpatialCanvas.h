2773c919464209a3248a98a96ebb2f697acc3fb1
Diego Ernst <dernst@xmartlabs.com>
2018-02-16 13:36:57 -0300

    version 0.5.3

---
+++ SpatialCanvas/SpatialCanvas.framework/Headers/SpatialCanvas.h
@@ -0,0 +1,19 @@
+//
+//  SpatialCanvas.h
+//  SpatialCanvas
+//
+//  Created by Diego Ernst on 10/5/17.
+//  Copyright © 2017 Xmartlabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SpatialCanvas.
+FOUNDATION_EXPORT double SpatialCanvasVersionNumber;
+
+//! Project version string for SpatialCanvas.
+FOUNDATION_EXPORT const unsigned char SpatialCanvasVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SpatialCanvas/PublicHeader.h>
+
+
